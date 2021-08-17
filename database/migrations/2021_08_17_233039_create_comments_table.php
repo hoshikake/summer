9cdb55446d5b37db94d986f3f0b71f88da49c890
funa074 <fnk074@gmail.com>
2021-08-17 23:35:19 +0900

    commentsテーブルを作成

---
+++ database/migrations/2021_08_17_233039_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->comment('投稿者ID');
+            $table->unsignedBigInteger('post_id')->comment('記事ID');
+            $table->text('comment')->comment('コメント');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
