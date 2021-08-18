0ccd7459c136f0598b8a8cf6e1dda3e3d840ec1c
funa074 <fnk074@gmail.com>
2021-08-18 23:43:50 +0900

    UserControllerとedit.blade.phpを作成

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+
+class UserController extends Controller
+{
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  Request $request
+     * @return View
+     */
+    public function edit(Request $request): View
+    {
+        /**
+         * @var User
+         */
+        $user = \Auth::user();
+        return view('edit', ['user' => $user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request $request
+     * @param  User $user
+     * @return RedirectResponse
+     */
+    public function update(Request $request, User $user): RedirectResponse
+    {
+        $user->twitter_id = $request->twitter_id;
+        $user->save();
+
+        return redirect()->route('edit')->with(['status' => 'プロフィール更新しました。']);
+    }
+}
