eadda4dd0324ae64ffdf9ed121335aa031340618
hosimiya7 <nerokarurin@gmail.com>
2021-08-19 12:20:19 +0900

    boby_color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,17 +29,12 @@
                 </div>
                 <div class="content-text">
                     <h2>企画内容</h2>
-                    <p><span>第一回：星駆水族館</span>　<span class="content-text-none">（制作期間：6/12～7/2　掲示期間：7/3～）</span></p>
-                    <p>第二回：企画中…</p>
+                    <a href="hoshikake.site">
+                        <p><span>第一回：星駆水族館</span> <span class="content-text-none">（制作期間：6/12～7/2　掲示期間：7/3～）</span></p>
+                    </a>
+                    <p><span>第二回：星駆花火大会・火死欠お化け屋敷</span> <span>（制作期間：7/26　掲示期間：8/29～）</span></p>
                 </div>
             </div>
-        </div>
-        <div class="bubbles">
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
         </div>
     </body>
 </html>
