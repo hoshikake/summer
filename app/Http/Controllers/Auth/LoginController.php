96f80d0434276de9b1ff35ebc64cc284c92bf06f
konagaya <cresta522@gmail.com>
2021-08-19 10:31:16 +0900

    認証テスト

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,7 +50,7 @@ class LoginController extends Controller
     public function handleProviderCallback($provider)
     {
         try {
-            dd($provider, Socialite::driver($provider)->stateless());
+            dd($provider, Socialite::driver($provider)->stateless()->user());
             $user = Socialite::driver($provider)->stateless()->user();
         } catch (Exception $e) {
             return redirect('/login');
