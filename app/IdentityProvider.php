97278cd834efbadcd983834fad00eba25a2fc050
konagaya <cresta522@gmail.com>
2021-08-19 10:35:39 +0900

    差分

--- app/IdentityProvider.php
+++ app/IdentityProvider.php
@@ -8,7 +8,7 @@ class IdentityProvider extends Model
 {
     protected $fillable = ['user_id', 'provider_name', 'provider_id'];
 
-    function user()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
