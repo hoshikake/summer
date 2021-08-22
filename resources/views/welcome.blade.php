efd2a1bcbf06845b5cd25eff18aeb6f1ef3ce448
hosimiya7 <nerokarurin@gmail.com>
2021-08-22 12:57:41 +0900

    link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                 <div class="content-text">
                     <h2>企画内容</h2>
                     {{-- リンク飛ばない --}}
-                    <a href="/hoshikake.site">
+                    <a href="http://hoshikake.site">
                         <p><span>第一回：星駆水族館</span> <span class="content-text-none">（制作期間：6/12～7/2　掲示期間：7/3～）</span></p>
                     </a>
                     <p><span>第二回：花火大会・お化け屋敷</span> <span class="content-text-none">（制作期間：7/26　掲示期間：8/29～）</span></p>
