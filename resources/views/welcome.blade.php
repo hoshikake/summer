12ea92efcf4bb0755cc3590a802aa5151ff3bad4
hosimiya7 <nerokarurin@gmail.com>
2021-08-26 11:10:11 +0900

    利用規約追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -165,6 +165,11 @@
                 <p><span>第一回：星駆水族館</span> <span class="content-text-none">（制作期間：6/12～7/2　掲示期間：7/3～）</span></p>
             </a>
             <p><span>第二回：花火大会・お化け屋敷</span> <span class="content-text-none">（制作期間：7/26　掲示期間：8/29～）</span></p>
+            <div class="rule">
+                <a href="https://github.com/sudo-roa/sudo-roa.github.io/blob/main/hoshikake_project/%E6%98%9F%E9%A7%86web%E5%88%B6%E4%BD%9C%E4%BC%81%E7%94%BB%E9%81%8B%E5%96%B6%E8%A6%8F%E7%B4%84.md"><p>運営規約</p></a>
+                <p>/</p>
+                <a href="https://github.com/sudo-roa/sudo-roa.github.io/blob/main/hoshikake_project/%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%AA%E3%82%B7%E3%83%BC.md"><p>プライバシーポリシー</p></a>
+            </div>
         </div>
     </div>
 
